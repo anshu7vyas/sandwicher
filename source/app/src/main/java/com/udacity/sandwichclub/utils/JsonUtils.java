70f7ca06db94e42437bc549be7d7fea4cdc53da8
av-7 <amanshulvyas@gmail.com>
2018-04-28 11:57:26 -0700

    Refactors JsonUtils.java

--- source/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ source/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -15,49 +15,63 @@ public class JsonUtils {
 
     private static final String LOG_TAG = JsonUtils.class.getSimpleName();
 
+    private static final String NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String DESCRIPTION = "description";
+    private static final String IMAGE = "image";
+    private static final String INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
 
+        JSONObject sandwichDetailsJsonObject;
+        String mainNameJson;
         List<String> alsoKnownAsJson = new ArrayList<>();
+        String placeOfOriginJson;
+        String descriptionJson;
+        String imageJson;
         List<String> ingredientsJson = new ArrayList<>();
+
         Sandwich mSandwich = new Sandwich();
 
         if (json!= null) {
             try {
-                JSONObject sandwichDetailsJsonObject = new JSONObject(json);
+                sandwichDetailsJsonObject = new JSONObject(json);
 
-                JSONObject nameJsonObject = sandwichDetailsJsonObject.getJSONObject("name");
+                JSONObject nameJsonObject = sandwichDetailsJsonObject.getJSONObject(NAME);
 
-                String mainNameJson = nameJsonObject.getString("mainName");
+                mainNameJson = nameJsonObject.getString(MAIN_NAME);
                 mSandwich.setMainName(mainNameJson);
                 Log.d(LOG_TAG, "Main Name = " + mainNameJson);
 
-                JSONArray sandwichAkaJsonArray = nameJsonObject.getJSONArray("alsoKnownAs");
+                JSONArray sandwichAkaJsonArray = nameJsonObject.getJSONArray(ALSO_KNOWN_AS);
                 if (sandwichAkaJsonArray != null) {
                     int len = sandwichAkaJsonArray.length();
                     for (int i = 0; i < len; i++) {
-                        alsoKnownAsJson.add(sandwichAkaJsonArray.get(i).toString());
+                        alsoKnownAsJson.add((String) sandwichAkaJsonArray.get(i));
                     }
                 }
                 mSandwich.setAlsoKnownAs(alsoKnownAsJson);
                 Log.d(LOG_TAG, "Also Known as = " + sandwichAkaJsonArray);
 
-                String placesOfOriginJson = sandwichDetailsJsonObject.getString("placeOfOrigin");
-                mSandwich.setPlaceOfOrigin(placesOfOriginJson);
-                Log.d(LOG_TAG, "Places of Origin = " + placesOfOriginJson);
+                placeOfOriginJson = sandwichDetailsJsonObject.getString(PLACE_OF_ORIGIN);
+                mSandwich.setPlaceOfOrigin(placeOfOriginJson);
+                Log.d(LOG_TAG, "Place of Origin = " + placeOfOriginJson);
 
-                String descriptionJson = sandwichDetailsJsonObject.getString("description");
+                descriptionJson = sandwichDetailsJsonObject.getString(DESCRIPTION);
                 mSandwich.setDescription(descriptionJson);
                 Log.d(LOG_TAG, "Description = " + descriptionJson);
 
-                String imageJson = sandwichDetailsJsonObject.getString("image");
+                imageJson = sandwichDetailsJsonObject.getString(IMAGE);
                 mSandwich.setImage(imageJson);
                 Log.d(LOG_TAG, "Image URL = " + imageJson);
 
-                JSONArray ingredientsJsonArray = sandwichDetailsJsonObject.getJSONArray("ingredients");
+                JSONArray ingredientsJsonArray = sandwichDetailsJsonObject.getJSONArray(INGREDIENTS);
                 if (ingredientsJsonArray != null) {
                     int len = ingredientsJsonArray.length();
                     for (int i = 0; i < len; i++) {
-                        ingredientsJson.add(ingredientsJsonArray.get(i).toString());
+                        ingredientsJson.add((String) ingredientsJsonArray.get(i));
                     }
                 }
                 mSandwich.setIngredients(ingredientsJson);
