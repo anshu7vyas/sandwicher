d3958b35ad2a9cba6f8c0feeb0d3e8ed6f50d235
av-7 <amanshulvyas@gmail.com>
2018-04-28 21:10:07 -0700

    completes populating the details activity, organizing the views

--- source/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ source/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,16 +1,21 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
 public class DetailActivity extends AppCompatActivity {
+
+    ActivityDetailBinding mBinding;
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
@@ -19,6 +24,8 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
@@ -43,7 +50,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +63,10 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        mBinding.originTv.setText((sandwich.getPlaceOfOrigin() == null) ? "" : sandwich.getPlaceOfOrigin());
+        mBinding.alsoKnownTv.setText((sandwich.getAlsoKnownAs() == null) ? "" : TextUtils.join(",", sandwich.getAlsoKnownAs()));
+        mBinding.alsoKnownTv.setText((sandwich.getIngredients() == null) ? "" : TextUtils.join(",", sandwich.getIngredients()));
+        mBinding.descriptionTv.setText((sandwich.getDescription() == null) ? "" : sandwich.getDescription());
     }
 }
